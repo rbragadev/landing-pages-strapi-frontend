2c63364701f43e96dc317ffd78c8c6ea04c4ce2f
Raphael Braga <raffael.info@gmail.com>
2023-11-17 16:26:21 -0300

    :rocket: feat: configuring styled components

--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,7 +1,11 @@
+import * as Styled from './styles';
+
 function Home() {
   return (
     <div className="App">
-      <h1>Home</h1>
+      <Styled.Wrapper background="blue">
+        <h1>Home</h1>
+      </Styled.Wrapper>
     </div>
   );
 }
