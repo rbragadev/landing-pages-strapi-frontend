730583e81359d4aba51f596af462c681ed3b1f7d
Raphael Braga <raffael.info@gmail.com>
2023-11-29 15:55:20 -0300

    :rocket: feat: tests heading

--- src/templates/App/App.test.jsx
+++ src/templates/App/App.test.jsx
@@ -1,15 +1,8 @@
 import Home from '.';
-import { screen } from '@testing-library/react';
 import { renderTheme } from '../../styles/render-theme';
-import { theme } from '../../styles/theme';
 
-test('renders learn react link', () => {
-  const { debug } = renderTheme(<Home />);
-  const headingContainer = screen.getByRole('heading', {
-    name: 'Hello',
-  }).parentElement;
-  expect(headingContainer).toHaveStyle({
-    background: theme.colors.primaryColor,
+describe('<Home />', () => {
+  it('should render home', () => {
+    renderTheme(<Home />);
   });
-  expect(headingContainer).toHaveStyleRule('background', 'white');
 });
